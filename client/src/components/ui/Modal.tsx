8322d6976e15ca2371f679ce4f30a953a935ae35
yuruyu6 <yuruyu6@gmail.com>
2021-10-16 00:26:15 +0300

    refactor Modal component (now use createPortal)

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -1,9 +1,11 @@
-import { Transition } from '@headlessui/react'
 import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
 interface ModalProps {
   isShowing: boolean
 }
+
+const modalRoot = document.querySelector('#modal-root')
 
 export const Modal: React.FC<ModalProps> = ({ children, isShowing }) => {
   useEffect(() => {
@@ -18,28 +20,24 @@ export const Modal: React.FC<ModalProps> = ({ children, isShowing }) => {
   }, [isShowing])
 
   return (
-    <Transition
-      show={isShowing}
-      enter="transition-opacity duration-75"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-150"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
-      <div className="modal bg-gray-700 bg-opacity-95">
-        <div className="flex justify-center items-center">
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
-          <div className="inline-block align-bottom py-8 px-12 text-gray-700 bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-            {children}
+    modalRoot &&
+    ReactDOM.createPortal(
+      isShowing && (
+        <div className="modal bg-gray-700 bg-opacity-95">
+          <div className="flex justify-center items-center">
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom py-8 px-12 text-gray-700 bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              {children}
+            </div>
           </div>
         </div>
-      </div>
-    </Transition>
+      ),
+      modalRoot
+    )
   )
 }
