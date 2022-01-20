4f37d66add8abdbe8686dc910aea71a3aae9b5f6
yuruyu6 <yuruyu6@gmail.com>
2022-01-20 11:57:07 +0200

    split translation; update translation for Loader

--- client/src/components/ui/Loader.tsx
+++ client/src/components/ui/Loader.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface LoaderProps {
   micro?: boolean
 }
 
 export const Loader: React.FC<LoaderProps> = ({ micro }) => {
+  const { t } = useTranslation()
+
   return (
     <div className="flex items-center justify-center px-2">
       <svg
@@ -31,7 +34,7 @@ export const Loader: React.FC<LoaderProps> = ({ micro }) => {
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
-      Loading...
+      {t('Loading')}...
     </div>
   )
 }
--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,80 +1,15 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import enTransition from './locales/en.json'
+import ruTransition from './locales/ru.json'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-      'Last update:': 'Last update:',
-      'ETH/USDT Rate': 'ETH/USDT Rate',
-      Export: 'Export',
-      Import: 'Import',
-      Summary: 'Summary',
-      'Add your equipment for tracking earnings':
-        'Add your equipment for tracking earnings',
-      'Information provided by HiveOn Pool':
-        'Information provided by HiveOn Pool',
-      daily: 'daily',
-      weekly: 'weekly',
-      monthly: 'monthly',
-      yearly: 'yearly',
-      'earnings per': 'earnings per',
-      Add: 'Add',
-      Name: 'Name',
-      Hashrate: 'Hashrate',
-      Back: 'Back',
-      Close: 'Close',
-      'Export data': 'Export data',
-      'Import data': 'Import data',
-      Copy: 'Copy',
-      'ExportDescription': 'Click the copy button below to save your data and use it to import to another device',
-      'ImportDescription': 'Paste saved string into the text field below to import your data',
-      'Incorrect export phrase': 'Incorrect export phrase',
-      'Rewards History': 'Rewards History',
-      'per': 'per',
-      'Show More': 'Show More',
-      'Expected Reward': 'Expected Reward',
-      'Date': 'Date'
-    },
+    translation: enTransition,
   },
   ru: {
-    translation: {
-      'Welcome to React': 'Bienvenue à React et react-i18next',
-      'Last update:': 'Последнее обновление:',
-      'ETH/USDT Rate': 'Курс ETH/USDT',
-      Export: 'Экспортировать настройки',
-      Import: 'Импортировать настройки',
-      Summary: 'Общее',
-      'Add your equipment for tracking earnings':
-        'Добавьте свое оборудование для отображения статистики',
-      'Information provided by HiveOn Pool':
-        'Информация предоставлена HiveOn Pool',
-      daily: 'дневные',
-      weekly: 'недельные',
-      monthly: 'месячные',
-      yearly: 'годовые',
-      'earnings per': 'вознаграждения за',
-      Add: 'Добавить',
-      Name: 'Название',
-      Hashrate: 'Хэшрейт',
-      Back: 'Назад',
-      Close: 'Закрыть',
-      'Export data': 'Экспортировать',
-      'Import data': 'Импортировать',
-      Copy: 'Копировать',
-      'ExportDescription': 'Скопируйте строку и используйте ее для импорта на другом устройстве', 
-      'ImportDescription': 'Вставьте сохраненную строку для импорта ранее сохраненных настроек',
-      'Incorrect export phrase': 'Строка некорректная',
-      'Rewards History': 'История вознаграждений',
-      'per': 'за',
-      'Show More': 'Показать больше',
-      'Expected Reward': 'ожидаемая награда',
-      'Date': 'Дата'
-    },
+    translation: ruTransition,
   },
 }
 
